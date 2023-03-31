e9b03108ed2f376c054d8fcb619b6533070eaf57
Nayab Ali <nayab8609@gmail.com>
2023-03-31 16:22:28 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 // ....................................................Landing page
-import Nav from "./Components/Nav";
-import Main from './Components/Main'
+//import Nav from "./Components/Nav";
+//import Main from './Components/Main'
 // ..............................................................
 // import Left from "./FB_CLONE/left";
-// import Nav from "./Microsoft Clone/nav";
-// import Slider from './Microsoft Clone/slider'
-// import Promo from './Microsoft Clone/promo'
+import Nav from "./Microsoft Clone/nav";
+import Slider from './Microsoft Clone/slider'
+import Promo from './Microsoft Clone/promo'
 function App() {
   return (
     <>
        {/* .......................................................landing page */}
-      <Nav/>
-     <Main/>
+      {/* <Nav/>
+     <Main/> */}
      {/* ................................................................... */}
       {/* ...........................................................................fb clone */}
-      {/* <Left /> */}
+       {/* <Left />  */}
       {/* ...............................................................................fb clone */}
-      {/* <Nav />
+       <Nav />
       <Slider/>
-      <Promo/> */}
+      <Promo/> 
       {/* ....................................................................................microsoft clone */}
       
     </>
